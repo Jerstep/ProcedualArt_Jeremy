28fe5b280e2c6a8929e335b36f80b866a4009652
Unknown <jeremy.koskamp@live.nl>
2018-11-29 01:15:35 +0100

    [+] Base Setup

---
+++ SoundScapeGen/Assets/Scripts/AudioReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+
+public class AudioReader : MonoBehaviour {
+
+    AudioSource _audioSource;
+    public float[] _samples = new float[512];
+
+	// Use this for initialization
+	void Start () {
+        _audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetSpectrumAusioSource();
+
+    }
+
+    void GetSpectrumAusioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+}
---
+++ SoundScapeGen/Assets/Scripts/GenerateWeardness.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateWeardness : MonoBehaviour {
+
+    public GameObject _prefabGameObject;
+    List<GameObject> _sampleGameObjects = new List<GameObject>();
+
+    bool _startMadness;
+
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start () {
+        coroutine = Wait(0.15f);
+        StartCoroutine(coroutine);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(_startMadness)
+        {
+            ObjectMadness();
+        }
+	}
+
+    private IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        _startMadness = true;
+    }
+
+    void ObjectMadness()
+    {
+
+    }
+}

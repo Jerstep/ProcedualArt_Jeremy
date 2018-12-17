e6fe29b3870c8512666d4b3caa0d57c7f5daa92d
Unknown <jeremy.koskamp@live.nl>
2018-12-17 01:36:09 +0100

    [+] Stuff

---
+++ SoundScapeGen/Assets/Scripts/FlowfieldParticle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowfieldParticle : MonoBehaviour {
+
+    public float moveSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += transform.forward * moveSpeed * Time.deltaTime;
+
+	}
+
+    public void ApplyRotation(Vector3 rotation, float rotationSpeed)
+    {
+        Quaternion targetRotation = Quaternion.LookRotation(rotation.normalized);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+}
